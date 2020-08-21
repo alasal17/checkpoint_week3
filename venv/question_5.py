42ee5c98fbaf8ed2a8e102f8f430becbc0d07267
alasal17 <salam.al.87@hotmail.com>
2020-08-21 12:45:27 +0200

    oppgave 5

--- venv/question_5.py
+++ venv/question_5.py
@@ -3,15 +3,32 @@ import random
 names = ['Camilla', 'Lene', 'Kaia', 'Helene', 'Malin', 'Osama', 'Carl', 'Martin', 'Salam']
 
 
+#Osama do not have glasses and cannot sit in the back row.
+#Salam get distracted by what going on outside the window and should not sit next to the windows
+
+
 list = [
     [],
     [],
     []
-
 ]
 
+def students_places():
+
+    try:
+            list[0].append(random.sample(names, 3))
+            list[1].append(random.sample(names, 3))
+            list[2].append(random.sample(names, 3))
+
+            if 'Salam' == list[0] or 'Salam' == list[1] or 'Salam' == list[2] and 'Osama' != list[2]:
+                list.sort()
+                print(list)
+            else:
+                print(list)
+    except Exception as e:
+        print(e)
+        pass
 
 
-print(list)
-
+students_places()
 
